fe3a4ed7fc97e2540a7da536be551efda574aca1
sa1am8 <tinkerino571@gmail.com>
2023-08-03 11:29:00 +0300

    project tests init

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,7 +1,3 @@
-import re
-from flask_mail import Message
-from flask import url_for
-
 from app import mail
 from app import models as m
 from app import db
@@ -12,62 +8,12 @@ TEST_EMAIL = "saintkos117@gmail.com"
 
 
 def test_auth_pages(client):
-    response = client.get("/register")
-    assert response.status_code == 200
     response = client.get("/login")
     assert response.status_code == 200
     response = client.get("/logout")
     assert response.status_code == 302
     response = client.get("/forgot")
     assert response.status_code == 200
-
-
-def test_register(client):
-    TEST_EMAIL = "sam@test.com"
-
-    with mail.record_messages() as outbox:
-        response = client.post(
-            "/register",
-            data=dict(
-                username="sam",
-                email=TEST_EMAIL,
-                password="password",
-                password_confirmation="password",
-            ),
-            follow_redirects=True,
-        )
-
-        assert response
-
-        assert (
-            b"Registration successful. Checkout you email for confirmation!."
-            in response.data
-        )
-
-        assert "toast" in response.data.decode()
-        assert "toast-success" in response.data.decode()
-        assert "toast-danger" not in response.data.decode()
-
-        user = db.session.query(m.User).filter_by(email=TEST_EMAIL).first()
-        assert user
-
-        assert len(outbox) == 1
-        letter: Message = outbox[0]
-        assert letter.subject == "New password"
-        assert "Confirm registration" in letter.html
-        assert user.unique_id in letter.html
-        html: str = letter.html
-
-        pattern = r"https?:\/\/[\w\d\.-]+\/activated\/[\w\d-]{36}"
-        urls = re.findall(pattern, html)
-        assert len(urls) == 1
-        url = urls[0]
-        response = client.get(url)
-        assert response.status_code == 302
-        response.location == url_for("main.index")
-        user: m.User = db.session.query(m.User).filter_by(email=TEST_EMAIL).first()
-        assert user
-        assert user.activated
 
 
 def test_forgot(client):
