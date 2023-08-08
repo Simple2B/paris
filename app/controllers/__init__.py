9b3359d2baffecf4b7612b75ba9cdf3644c6fdc0
sa1am8 <tinkerino571@gmail.com>
2023-08-08 17:31:24 +0300

    removing get_browser

--- app/controllers/__init__.py
+++ app/controllers/__init__.py
@@ -1,6 +1,5 @@
 # flake8: noqa F401
 from .pagination import create_pagination
-from .selenium import get_browser
 from .ticket import add_ticket_date, add_ticket_time
 from .browser import go
 from .celery import bot as processing_task
