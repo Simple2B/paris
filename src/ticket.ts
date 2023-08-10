b39cfca479cc63ed46b6fcf6e98f6c007042dc2c
marynag <mgrabchak1109@gmail.com>
2023-08-10 13:07:32 +0300

    add: order tickets pop up

--- src/ticket.ts
+++ src/ticket.ts
@@ -1,1 +1,37 @@
 // ticket.ts
+
+document.addEventListener('DOMContentLoaded', function() {
+    const modalToggleButtons = document.querySelectorAll('[data-modal-toggle="add-ticket-modal"]');
+    const floorNamePlaceholder = document.getElementById('floorNamePlaceholder');
+    
+    modalToggleButtons.forEach(button => {
+        button.addEventListener('click', function() {
+            const floorName = button.getAttribute('data-floor-name');
+            floorNamePlaceholder.textContent = floorName; 
+        });
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const modalToggleButtons = document.querySelectorAll('[data-modal-toggle="add-ticket-modal"]');
+    const timePlaceholder = document.getElementById('timePlaceholder');
+    
+    modalToggleButtons.forEach(button => {
+        button.addEventListener('click', function() {
+            const time = button.getAttribute('date-time');
+            timePlaceholder.textContent = time;
+        });
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const modalToggleButtons = document.querySelectorAll('[data-modal-toggle="add-ticket-modal"]');
+    const datePlaceholder = document.getElementById('datePlaceholder');
+    
+    modalToggleButtons.forEach(button => {
+        button.addEventListener('click', function() {
+            const date = button.getAttribute('data-date');
+            datePlaceholder.textContent = date;
+        });
+    });
+});
